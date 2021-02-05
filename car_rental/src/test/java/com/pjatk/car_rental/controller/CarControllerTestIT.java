6abead0a41e94c0099ee0c898fb13c656540f095
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2021-02-05 18:23:47 +0100

    few controllers tests

--- car_rental/src/test/java/com/pjatk/car_rental/controller/CarControllerTestIT.java
+++ car_rental/src/test/java/com/pjatk/car_rental/controller/CarControllerTestIT.java
@@ -1,12 +1,27 @@
 package com.pjatk.car_rental.controller;
 
+import com.pjatk.car_rental.model.Car;
+import com.pjatk.car_rental.repository.CarRepository;
+import com.pjatk.car_rental.service.CarService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -15,8 +30,63 @@ public class CarControllerTestIT {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private CarService carService;
+
+    @MockBean
+    private CarRepository carRepository;
+
     @Test
-    void findAll() throws Exception{
-        mockMvc.perform(get("/car")).andDo(print()).andExpect(status().isOk());
+    void shouldFindAll() throws Exception{
+        Date date1 = new Date(2020-01-01);
+        Car car = new Car("bmw","a","aaa",date1,0);
+        List<Car> carList = new ArrayList<>();
+        carList.add(car);
+
+        when(carRepository.findAll()).thenReturn(carList);
+        mockMvc.perform(get("/car/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("{\"id\":1,\"brand\":\"mini\",\"model\":\"1\",\"plate\":\"a\",\"manufacture_date\":\"2020-01-01\",\"rented\":0}")));
     }
+
+    @Test
+    void shouldDeleteById() throws Exception{
+        Date date1 = new Date(2020-01-01);
+        Optional<Car> car = Optional.of(new Car(1L,"mini","1","a",date1,0));
+
+        when(carRepository.findById(car.get().getId())).thenReturn(car);
+        mockMvc.perform(delete("/car/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("")));
+    }
+
+    @Test
+    void shouldDeleteAll() throws Exception{
+        Date date1 = new Date(2020-01-01);
+        Date date2 = new Date(2000-01-01);
+        Car car1 = new Car("bmw","a","aaa",date1,0);
+        Car car2 = new Car(1L,"bmw","a","aaa",date2,0);
+        List<Car> carList = new ArrayList<>();
+        carList.add(car1);
+        carList.add(car2);
+
+
+        when(carRepository.findAll()).thenReturn(carList);
+        mockMvc.perform(delete("/car"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("")));
+    }
+
+    @Test
+    void shouldFindCarById() throws Exception{
+        Date date1 = new Date(2020-01-01);
+        Optional<Car> car = Optional.of(new Car(1L,"mini","1","a",date1,0));
+
+        when(carRepository.findById(car.get().getId())).thenReturn(car);
+        mockMvc.perform(get("/car/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("{\"id\":1,\"brand\":\"mini\",\"model\":\"1\",\"plate\":\"a\",\"manufacture_date\":\"2020-01-01\",\"rented\":0}")));
+    }
+
+
 }
--- car_rental/src/test/java/com/pjatk/car_rental/controller/CustomerControllerTestIT.java
+++ car_rental/src/test/java/com/pjatk/car_rental/controller/CustomerControllerTestIT.java
@@ -1,13 +1,27 @@
 package com.pjatk.car_rental.controller;
 
+import com.pjatk.car_rental.model.Car;
+import com.pjatk.car_rental.model.Customer;
+import com.pjatk.car_rental.repository.CustomerRepository;
+import com.pjatk.car_rental.service.CustomerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -16,8 +30,112 @@ public class CustomerControllerTestIT {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private CustomerService customerService;
+
+    @MockBean
+    private CustomerRepository customerRepository;
+
+
+
     @Test
-    void findAll() throws Exception{
-        mockMvc.perform(get("/customer")).andDo(print()).andExpect(status().isOk());
+    void shouldFindAll() throws Exception{
+        Customer customer1 = new Customer(1L,"a","b","111111",0);
+        Customer customer2 = new Customer(2L,"c","b","222222",3);
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(customer1);
+        customerList.add(customer2);
+
+
+        when(customerRepository.findAll()).thenReturn(customerList);
+        mockMvc.perform(get("/customer"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"a\",\"surname\":\"b\",\"phone_number\":\"111111\",\"rents\":0},{\"id\":2,\"name\":\"c\",\"surname\":\"b\",\"phone_number\":\"222222\",\"rents\":3}]")));
+    }
+
+
+    @Test
+    void shouldDeleteById() throws Exception{
+        Optional<Customer> customer = Optional.of(new Customer(1L,"a","b","111",0));
+
+        when(customerRepository.findById(customer.get().getId())).thenReturn(customer);
+        mockMvc.perform(delete("/customer/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("")));
+    }
+
+
+    @Test
+    void shouldDeleteAll() throws Exception{
+        Customer customer1 = new Customer("a","b","111111",0);
+        Customer customer2 = new Customer("c","b","222222",3);
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(customer1);
+        customerList.add(customer2);
+
+
+        when(customerRepository.findAll()).thenReturn(customerList);
+        mockMvc.perform(delete("/customer"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("")));
+    }
+
+    @Test
+    void shouldFindCustomerById() throws Exception{
+        Optional<Customer> customer1 = Optional.of(new Customer(1L,"a", "b", "111111", 0));
+
+        when(customerRepository.findById(customer1.get().getId())).thenReturn(customer1);
+        mockMvc.perform(get("/customer/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("{\"id\":1,\"name\":\"a\",\"surname\":\"b\",\"phone_number\":\"111111\",\"rents\":0}")));
+    }
+
+
+    @Test
+    void shouldShowMoreThanXRents() throws Exception{
+        Customer customer1 = new Customer(1L,"a","b","111111",0);
+        Customer customer2 = new Customer(2L,"c","b","222222",3);
+        customerService.saveCustomer(customer1);
+        customerService.saveCustomer(customer2);
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(customer1);
+        customerList.add(customer2);
+
+        when(customerRepository.findAll()).thenReturn(customerList);
+        mockMvc.perform(get("/more/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("{\"id\":2,\"name\":\"c\",\"surname\":\"b\",\"phone_number\":\"222222\",\"rents\":3}")));
+    }
+
+    @Test
+    void shouldShowLessThanXRents() throws Exception{
+        Customer customer1 = new Customer(1L,"a","b","111111",0);
+        Customer customer2 = new Customer(2L,"c","b","222222",3);
+        customerService.saveCustomer(customer1);
+        customerService.saveCustomer(customer2);
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(customer1);
+        customerList.add(customer2);
+
+        when(customerRepository.findAll()).thenReturn(customerList);
+        mockMvc.perform(get("/less/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("{\"id\":1,\"name\":\"a\",\"surname\":\"b\",\"phone_number\":\"111111\",\"rents\":0}")));
+    }
+
+    @Test
+    void shouldShowExactlyXRents() throws Exception{
+        Customer customer1 = new Customer(1L,"a","b","111111",0);
+        Customer customer2 = new Customer(2L,"c","b","222222",3);
+        customerService.saveCustomer(customer1);
+        customerService.saveCustomer(customer2);
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(customer1);
+        customerList.add(customer2);
+
+        when(customerRepository.findAll()).thenReturn(customerList);
+        mockMvc.perform(get("/equal/0"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("{\"id\":1,\"name\":\"a\",\"surname\":\"b\",\"phone_number\":\"111111\",\"rents\":0}")));
     }
 }
